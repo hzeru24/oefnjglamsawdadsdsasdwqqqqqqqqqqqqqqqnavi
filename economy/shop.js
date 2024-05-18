25462c13cd43f066576c9a2cf6150838ca7f24a8
Crack brained <102344051+nobraine13@users.noreply.github.com>
2024-05-18 18:03:21 +0800

    Add files via upload

---
+++ economy/shop.js
@@ -0,0 +1,258 @@
+module.exports = [
+  {
+    items: "banknote",
+    price: {buy: 325000, sell: 0},
+    desc: "You can either use this note to expand your bank space or sell it for some coins.",
+    emoji: "<:nkn:938365740422144010>",
+    type: "tools",
+    rarity: "Common"
+  },
+  {
+    items: "camera",
+    price: {buy: 20000, sell: 0},
+    desc: "Used to capture images.",
+    emoji: "<:cam:938262856095391814>",
+    type: "tools",
+    rarity: "Common"
+  },
+  {
+    items: "dubiousfood",
+    price: {buy: 0, sell: 69420},
+    desc: "It's too gross to even look at. A bizarre smell issues forth from this heap.",
+    emoji: "<:dubious_food:941586416687788032>",
+    type: "food",
+    rarity: "Uncommon"
+  },
+  {
+    items: "meatskewer",
+    price: {buy: 0, sell: 7500},
+    desc: "A juicy, filling snack made by grilling small chunks of meat on a skewer.",
+    emoji: "<:meat_skewer:943746389878390804>",
+    type: "tools",
+    rarity: "Common"
+  },
+  {
+    items: "rawmeat",
+    price: {buy: 0, sell: 3500},
+    desc: "Meat obtained from animals in plains and forest. You can eat it raw, but careful preparation will improve the flavor substantially.",
+    emoji: "<:raw_meat:941581658719666237>",
+    type: "materials",
+    rarity: "Common"
+  },
+  {
+    items: "soupladle",
+    price: {buy: 35000, sell: 0},
+    desc: "A kitchen implement often used for serving delicious soups. It was carved from the wood of a sturdy tree, so it actually packs quite the wallop.",
+    emoji: "<:soup_ladle:943742047876153404>",
+    type: "tools",
+    rarity: "Common"
+  },
+  {
+    items: "starfragment",
+    price: {buy: 0, sell: 50000},
+    desc: "A sparkly stone that fell from the sky. It's said to be a piece of a star.",
+    emoji: "<:star_fragment:942269974939058176>",
+    type: "treasure",
+    rarity: "Uncommon"
+  },
+  {
+    items: "apple",
+    price: {buy: 0, sell: 5000},
+    desc: "A common fruit found on trees all around Hyrule.",
+    emoji: "<:apple:941639034319568956>",
+    type: "materials",
+    rarity: "Uncommon"
+  },
+  {
+    items: "mightybanana",
+    price: {buy: 0, sell: 7500},
+    desc: "This fruit grows mainly in tropical forests of the Faron region.",
+    emoji: "<:mighty_banana:941639271759085608>",
+    type: "materials",
+    rarity: "Uncommon"
+  },
+  {
+    items: "fairy",
+    price: {buy: 0, sell: 5000},
+    desc: "It is easily mistaken for a firefly at first glance, but it glows brightly both at night and during the day.",
+    emoji: "<:fairy:942284545401696266>",
+    type: "collectables",
+    rarity: "Uncommon"
+  },
+  {
+    items: "goronspice",
+    price: {buy: 0, sell: 15000},
+    desc: "Made from several types of spices, this secret Goron seasoning has been handed down for generations. An initial wave of spiciness paves the way for the sweetness.",
+    emoji: "<:goron_spice:942273387689017364>",
+    type: "materials",
+    rarity: "Uncommon"
+  },
+  {
+    items: "flint",
+    price: {buy: 0, sell: 10000},
+    desc: "Strike it with a metallic weapon to generate a spark.",
+    emoji: "<:flint:942298515277774900>",
+    type: "materials",
+    rarity: "Uncommon"
+  },
+  {
+    items: "hylianshroom",
+    price: {buy: 0, sell: 5000},
+    desc: "A common mushroom found near trees around Hyrule.",
+    emoji: "<:hylian_shroom:942290785573941309>",
+    type: "materials",
+    rarity: "Uncommon"
+  },
+  {
+    items: "wood",
+    price: {buy: 0, sell: 10000},
+    desc: "Useful...",
+    emoji: "<:wood:942228764132274176>",
+    type: "materials",
+    rarity: "Common"
+  },
+  {
+    items: "emblem",
+    price: {buy: 125000000, sell: 0},
+    desc: "Used to flex on users who can't afford one.",
+    emoji: "<a:emblem:939404336176435240>",
+    type: "collectable",
+    rarity: "Rare"
+  },
+  {
+    items: "blob",
+    price: {buy: 500000000, sell: 0},
+    desc: "This is just a shell of it's former self, earned for doing basically absolutely nothing during the blob event",
+    emoji: "<a:blob:938366352161402910>",
+    type: "collectable",
+    rarity: "Legendary"
+  },
+  {
+    items: "laptop",
+    price: {buy: 5000, sell: 500},
+    desc: "Earn money by posting memes!",
+    emoji: "<:laptop:938405811590402068>",
+    type: "tools",
+    rarity: "Common"
+  },
+  {
+    items: "crown",
+    price: {buy: 250000000, sell: 0},
+    desc: "Only the richest player have this item",
+    emoji: "<:royalcrown:938270588772941834>",
+    type: "collectable",
+    rarity: "Rare"
+  },
+  {
+    items: "shield",
+    price: {buy: 5000, sell: 2300},
+    desc: "Protect your wallet with shield to prevent people from robbing you. This is a one-time use item; once it's broken, it won't come back or do anything unless you buy another one.",
+    emoji: "<:shield:938367816782991360>",
+    type: "tools",
+    rarity: "Rare"
+  },
+  {
+    items: "fishingrod",
+    price: {buy: 25000, sell: 1200},
+    desc: "This fishing rod allows you to use the fish command, which can give you lots of different types of items!",
+    emoji: "<:fishingpole:938364171257516134>",
+    type: "tools",
+    rarity: "Common"
+  },
+  {
+    items: "sword",
+    price: {buy: 25000, sell: 1200},
+    desc: "This sword allows you to use the hunt command, which can give you lots of different types of items!",
+    emoji: "<:sword:938367686944112670>",
+    type: "tools",
+    rarity: "Common"
+  },
+  {
+    items: 'commonfish',
+    price: {buy: 0, sell: 250000},
+    desc: "This item's purpose is to be collected or sold. Nothing more, nothing less.",
+    emoji: '<:commonfish:938361938814070794>',
+    type: 'sellable',
+    rarity: 'Common'
+  },
+  {
+    items: 'rarefish',
+    price: {buy: 0, sell: 750000},
+    desc: "This item's purpose is to be collected or sold. Nothing more, nothing less.",
+    emoji: '<:rarefish:938358000857542700>',
+    type: 'sellable',
+    rarity: 'Rare'
+  },
+  {
+    items: 'legendaryfish',
+    price: {buy: 0, sell: 2500000},
+    desc: "This item's purpose is to be collected or sold. Nothing more, nothing less.",
+    emoji: '<:goldenfish:938360875180519445>',
+    type: 'sellable',
+    rarity: 'Legendary'
+  },
+  {
+    items: 'ladybug',
+    price: {buy: 0, sell: 1800},
+    desc: "This item's purpose is to be collected or sold. Nothing more, nothing less.",
+    emoji: '🐞',
+    type: 'sellable',
+    rarity: 'Common'
+  },
+  {
+    items: 'bunny',
+    price: {buy: 0, sell: 6000},
+    desc: "This item's purpose is to be collected or sold. Nothing more, nothing less.",
+    emoji: '🐇',
+    type: 'sellable',
+    rarity: 'Uncommon'
+  },
+  {
+    items: 'deer',
+    price: {buy: 0, sell: 6900},
+    desc: "This item's purpose is to be collected or sold. Nothing more, nothing less.",
+    emoji: '🦌',
+    type: 'sellable',
+    rarity: 'Rare'
+  },
+  {
+    items: 'santasbag',
+    price: {buy: 0, sell: 0},
+    desc: 'You never know what Santa keeps in his bag',
+    emoji: '🎒',
+    type: 'lootbox',
+    rarity: 'Very Rare'
+  },
+  {
+    items: 'chest',
+    price: {buy: 2500000, sell: 0},
+    desc: "I wonder what's inside",
+    emoji: '<:chest:938378267633803304>',
+    type: 'lootbox',
+    rarity: 'Very Rare'
+  },
+    {
+    items: 'spinner',
+    price: {buy: 500000, sell: 0},
+    desc: 'Used to increase your chances in gambling.',
+    emoji: '<a:fidgetspinner:938714093748121600>',
+    type: 'useable',
+    rarity: 'Uncommon'
+  },
+      {
+    items: 'present',
+    price: {buy: 200000, sell: 0},
+    desc: 'Contains coins and ...',
+    emoji: '<:present:938266333848666132>',
+    type: 'lootbox',
+    rarity: 'Rare'
+      },
+  {
+    items: 'snowball',
+    price: {buy: 0, sell: 450000},
+    desc: 'A packed of snow in a shape of a ball.',
+    emoji: '❄',
+    type: 'sellable',
+    rarity: 'Very Rare'
+  }
+]
